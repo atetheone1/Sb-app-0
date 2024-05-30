0ed5b9d14513bf1ca821f0bad3657b2605a46882
atetheone <atevirran@gmail.com>
2024-05-30 19:10:11 +0000

    Spring Boot initialized

---
+++ src/main/java/tech/atetheone/Sb_app_0/SbApp0Application.java
@@ -0,0 +1,13 @@
+package tech.atetheone.Sb_app_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp0Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/atetheone/Sb_app_0/SbApp0ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.atetheone.Sb_app_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApp0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
